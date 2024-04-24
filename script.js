c5e6a2e02a3152ce7e57c3021ba6081ba74a0386
joaoburi <joaoburi64@gmail.com>
2024-04-24 18:10:39 -0300

    Menu funcionando

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function menuAparecer() {
-    let menuMobile = document.querySelector('#mobile-menu');
+    let menuMobile = document.querySelector('.mobile-menu')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('.icone').src = "imagens/icone1.png";
     } else {
         menuMobile.classList.add('open');
+        document.querySelector('.icone').src = "imagens/icone3.png";
     }
 }
